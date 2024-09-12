9e0dc0b657f0a7374984d4663b2bc52f1dafe000
UfukTopalan <double123416@gmail.com>
2024-09-12 13:32:27 +0300

    Create Snmf_imputation.sh

---
+++ scripts_folder/Snmf_imputation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=LFMM
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --partition=long
+#SBATCH --mem=200G
+#SBATCH --time=150:00:00
+#SBATCH --output=lfmm-%j.out
+
+
+module load R/4.2.0
+module load mambaforge
+source activate
+conda activate R
+
+Rscript Snmf_imputation.R
